072b7a64c8d4b1e5e3657d6065ba99fda9fa6f02
Matheus-A-Santana <matheus.santana7@hotmail.com>
2024-01-16 01:31:40 -0300

    iniciando projeto do cara ou coroa

---
+++ Cara ou Coroa/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Cara_ou_Coroa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            switch(Convert.ToInt64(Console.ReadLine()))
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    
+                    break;
+            }
+        }
+    }
+}
