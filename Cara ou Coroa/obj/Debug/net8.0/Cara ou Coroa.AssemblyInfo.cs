c8b53ad6ef2f0ef60e2120c10a5408a9881f1d98
Matheus-A-Santana <matheus.santana7@hotmail.com>
2024-01-16 21:18:28 -0300

    adicionando comando para limpar a tela do terminal

--- Cara ou Coroa/obj/Debug/net8.0/Cara ou Coroa.AssemblyInfo.cs
+++ Cara ou Coroa/obj/Debug/net8.0/Cara ou Coroa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cara ou Coroa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072b7a64c8d4b1e5e3657d6065ba99fda9fa6f02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4bc8495d2898da9076b4d8475e342e08724318")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cara ou Coroa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cara ou Coroa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
